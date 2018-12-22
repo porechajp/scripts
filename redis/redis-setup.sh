a28f4fac98391dfa0b49cf4f40e3db679867253d
Jatan Porecha <porecha.jp@gmail.com>
2018-12-22 12:46:19 +0530

    Redist setup script for WSL Ubuntu 18.04

---
+++ redis/redis-setup.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+apt update
+apt install gcc -y -q
+wget -O /tmp/redis.tar.gz http://download.redis.io/releases/redis-5.0.3.tar.gz
+mkdir /opt/redis
+tar xvf /tmp/redis.tar.gz --directory /opt/redis --strip 1
+apt install make -y -q
+cd /opt/redis
+make
+sed -e '/save/ s/^#*/#/' -i /opt/redis/redis.conf
+sed -e 's#dump.rdb#/tmp/dump.rdb#' -i /opt/redis/redis.conf
+ln -s /opt/redis/src/redis-server /bin/redis-server
+ln -s /opt/redis/src/redis-cli /bin/redis-cli
+
+
